40df8378f842a5ce7dcd7f17642267152f28d329
rsiew11 <rsiew11@gmail.com>
2017-12-06 08:22:35 +0000

    modified IP too

--- test_send_to_computer.sh
+++ test_send_to_computer.sh
@@ -4,7 +4,7 @@ DESTDIR=~/743scripts/images/$1/test_img.jpg
 
 raspistill -vf -hf -o $DESTDIR
 
-nc -q 0 128.237.183.153 1234 < $DESTDIR
+nc -q 0 128.237.138.63 1234 < $DESTDIR
 
 nc -l 1234 > results.txt
 
