e0decea3ee80d3ef2bd1e550dd237f5efb62464d
rsiew11 <rsiew11@gmail.com>
2017-12-06 07:24:51 +0000

    slight mods

--- train_local.sh
+++ train_local.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 DESTDIR=$1
-RESULTS=~/743scripts/results/$DESTDIR
+RESULTS=./results/$DESTDIR
 
 mkdir $RESULTS
 
-python ../tensorflow/tensorflow/examples/image_retraining/retrain.py --bottleneck_dir=$RESULTS/bottlenecks --how_many_training_steps 500 --model_dir=../inception --output_graph=$RESULTS/retrained_graph.pb --output_labels=$RESULTS/retrained_labels.txt -output_labels=$RESULTS/retrained_labels.txt --image_dir ~/743scripts/images/$DESTDIR
+echo "made folder"
+
+python ../tensorflow/tensorflow/examples/image_retraining/retrain.py --bottleneck_dir=$RESULTS/bottlenecks --how_many_training_steps 500 --model_dir=../inception --output_graph=$RESULTS/retrained_graph.pb --output_labels=$RESULTS/retrained_labels.txt -output_labels=$RESULTS/retrained_labels.txt --image_dir ./images/$DESTDIR
